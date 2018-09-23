39d77e7a07f0bf9a3959e22fe44cf187af8424f3
Deniel Pereira <deniel_16@live.com>
2018-09-23 12:15:39 -0300

    Altera extensão dos arquivos e remove codigo redundante

--- views/index.php
+++ views/index.php
@@ -3,14 +3,6 @@
 
 <head>
   <?php include './partials/head.html';?>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="../favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
-  <link rel="stylesheet" href="../css/index.css">
-  <link rel="stylesheet" href="../assets/bootstrap/bootstrap.min.css">
-  <script src="../assets/bootstrap/bootstrap.min.js"></script>
-  <title>FCCR | Eventos</title>
 </head>
 
 <body>
