8c57533f6131ddc265b666e1b5fe0b0d31fea47f
Deniel Pereira <deniel_16@live.com>
2018-10-08 15:29:32 -0300

    Estiliza a tela de confirmação

--- views/desativar-evento.php
+++ views/desativar-evento.php
@@ -6,20 +6,35 @@
     include_once("./../src/controllers/pegarevento.php");
 ?>
 
-
-<html>
-<form class="w-80 mx-auto centered" action="../src/controllers/desativarevento.php" method="POST">
-<div class="form-group">
-                        <div class="form-check  form-check-inline">
-                        <input class="form-check-input" type="hidden" name="situacao" value="0" /> <input class="form-check-input" type="checkbox" name="situacao" id="checkbox" value="1"
-                                checked>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include './partials/head.php';?>
+</head>
+<body class="bg-success">
+    <div class="block h-100">
+        <div class="card mx-auto mt-5" style="width: 40vw;">
+            <div class="card-body">
+                <h5 class="card-title">Você tem certeza disso?</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Isso não poderá ser desfeito.</h6>
+                <form class="w-80 mx-auto " action="../src/controllers/desativarevento.php" method="POST">
+                    <div class="form-group">
+                        <div class="form-check form-check-inline mt-3">
+                            <input class="form-check-input" type="hidden" name="situacao" value="0" /> <input class="custom-control-input form-check-input" type="checkbox" name="situacao" id="checkbox" value="1" checked>
                             <label class="form-check-label" for="checkbox">
-                                Este evento será desativado e não poderá mais ser visto.
+                                    Este evento será <b>EXCLUÍDO</b> e não poderá mais ser visto por ninguém. 
                             </label>
                         </div>
+                        <div class="button mt-5">
+                            <button class="btn btn-success" onclick="window.location.href='/eventos.php'">Voltar</button>
+                            <button type="submit" class="btn btn-danger ml-3">Sim, eu tenho certeza</button>
+                        </div>
                     </div>
-                    <button type="submit" class="btn btn-success">Desativar</button>
-</form>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
 </html>
 
-                
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
