dabbc7c5305ae1b1b9ade08a2c92b21eb663029b
Igor <igor.serafimsantos@gmail.com>
2018-10-13 22:59:40 -0300

    ordenando comentário do mais novo pro mais velho

--- src/models/ComentarioDAO.php
+++ src/models/ComentarioDAO.php
@@ -25,8 +25,8 @@ class ComentarioDAO {
 
     public function getComentarioByEvento($id) {
         try {
-            $sql = "SELECT eventos_id, comentario, usuario_id, usuario.nome, usuario.sexo FROM comentario
-            JOIN usuario ON comentario.usuario_id = usuario.id WHERE eventos_id = '$id'";
+            $sql = "SELECT eventos_id, comentario, usuario_id, usuario.nome, usuario.sexo, contador FROM comentario
+            JOIN usuario ON comentario.usuario_id = usuario.id WHERE eventos_id = '$id' ORDER BY contador DESC";
             $result = $this->_conexaoDB->query($sql);
             $rows = $result->fetchAll();
             if($rows) {
