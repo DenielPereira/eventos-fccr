a5b9490cfe0b570feb6ea16326a4975cc08afa48
Deniel Pereira <deniel_16@live.com>
2018-11-10 22:49:30 -0200

    corrige bug que alterava extensão da foto

--- src/models/ImagemDAO.php
+++ src/models/ImagemDAO.php
@@ -20,58 +20,61 @@ class ImagemDAO {
             
             $arquivo = $imagem->getArquivo();
             
-            ini_set( 'display_errors', true );
-            error_reporting( E_ALL );
+            //ini_set( 'display_errors', true );
+            //error_reporting( E_ALL );
 
             if(isset($arquivo)){
-                $errors= array();
-                $file_name = $imagem->getNome();;
-                $file_size = $_FILES['arquivo']['size'];
-                $file_tmp = $imagem->getConteudo();
-                $file_type = $_FILES['arquivo']['type'];
-                $file_ext = strtolower(end(explode('.',$file_name)));  
-        
-                $usuario = $_SESSION['id'];
-        
-                $expensions = array("jpeg","jpg","png");
 
-                $path = "../../upload/".$evento."/".$usuario;
-        
-                if (!file_exists($path)) {
-                    mkdir($path, 0777, true);
-                }
-  
-                if(in_array($file_ext,$expensions)=== false){
-                    $errors[]="Extensão de imagem não permitida, por favor escolha um arquivo JPEG ou PNG.";
-                }
-        
-                if($file_size > 2097152) {
-                    $errors[]='Erro! O tamanho da imagem deve ser menor do que 2 MB';
-                } 
-        
-      
-                if(empty($errors)==true) {
-                    $file_name = $file_name.$evento."-".$usuario;
-                    $file_path = $path."/".$file_name;
-              
-                    if(file_exists($file_path)){
-                        echo "<script>alert('Uma imagem com este nome já foi cadastrada por você neste evento! Mude o nome da imagem.');</script>";
-                        echo "<script>window.location.href = './../../views/checkin.php?id=".$evento."';</script>";
-        
-                    } else {
-                        $upload = move_uploaded_file($file_tmp,"../../upload/".$evento."/".$usuario."/".$file_name);
-                        
-                        if ($upload) {
-                            $imagemDAO->insertTable($file_path, $evento, $usuario);
-                            echo "<script>alert('A imagem foi cadastrada com sucesso!');</script>";
-                            echo "<script>window.location.href = './../../views/evento.php?id=".$evento."';</script>";
-                        }
-                    }
-            
-                } else {
-                print_r($errors);
-                }
-            }
+    $errors = null;
+    $file_name = $_FILES['arquivo']['name'];
+    $file_size = $_FILES['arquivo']['size'];
+    $file_tmp = $_FILES['arquivo']['tmp_name'];
+    $file_type = $_FILES['arquivo']['type'];
+    $file_ext = strtolower(end(explode('.',$_FILES['arquivo']['name'])));  
+    $file_new_name = (explode('.',$_FILES['arquivo']['name']));         
+    
+    $evento = $_GET['id'];
+    $usuario = $_SESSION['id'];
+    $file_name = $file_new_name[0];
+    
+    $expensions = array("jpeg","jpg","png");
+    
+    $path = "../../upload/".$evento."/".$usuario;
+    
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
+    
+    if(in_array($file_ext,$expensions)=== false){
+        $errors="Isso não se parece com uma imagem... 
+        Apenas imagens podem ser enviadas.";
+    }
+    
+    if($file_size > 2097152) {
+        $errors="A sua foto é muito grande! <br>
+        Infelizmente arquivos com mais de 2MB não podem ser enviados :( <br> 
+        Tente reduzir o tamanho da foto. <br> ";
+    // Eu posso te indicar <a href="."https://www.easy-resize.com/pt/"." target="."_blank".">esse site</a>, mas você pode fazer do jeito que achar melhor :)";
+    exit;
+    } 
+    
+    
+    if(empty($errors)==true) {
+        $file_name = $file_name."(e".$evento."u".$usuario.")".".".$file_ext;
+        $file_path = $path."/".$file_name;
+        if(file_exists($file_path)){
+            echo "<script>alert ('Parece que a gente já tem um arquivo com esse mesmo nome :(  Se você tiver certeza de que não é a mesma foto, tente enviar de novo com um nome diferente.');</script>";
+            echo "<script>javascript:history.back()</script>";
+        }else{
+            move_uploaded_file($file_tmp,"../../upload/".$evento."/".$usuario."/".$file_name);
+            $imagemDAO->insertTable($file_path, $evento, $usuario);
+            echo "<script>javascript:history.back()</script>";
+        }
+    }else{
+        echo "<script>alert ('".$errors."');</script>";
+        echo "<script>javascript:history.back()</script>";
+    }
+}
 
         } catch(PDOException $e) {
         echo "Falha: {$e}";
