ba8a046869f9b89c7a70379c41c4249e4f0b3c0f
Deniel Pereira <deniel_16@live.com>
2018-11-11 21:11:24 -0200

    aprimora visualização de confirmação de exclusão de eventos

--- views/desativar-evento.php
+++ views/desativar-evento.php
@@ -11,30 +11,50 @@
 <head>
     <?php include './partials/head.php';?>
 </head>
-<body class="bg-success">
-    <div class="block h-100">
-        <div class="card mx-auto" style="width: 40vw; margin-top: 10%">
-            <div class="card-body">
-                <h5 class="card-title">Você tem certeza disso?</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Isso não poderá ser desfeito.</h6>
-                <form class="w-80 mx-auto " action="../src/controllers/desativarevento.php" method="POST">
-                    <div class="form-group">
-                        <div class="form-check form-check-inline mt-3">
-                            <input class="form-check-input" type="hidden" name="situacao" value="0" /> <input class="custom-control-input form-check-input" type="checkbox" name="situacao" id="checkbox" value="1" checked>
-                            <label class="form-check-label" for="checkbox">
-                                    Este evento será <b>EXCLUÍDO</b> e não poderá mais ser visto por ninguém. 
-                            </label>
-                        </div>
-                        <div class="button mt-5">
-                            <button type="submit" class=" btn btn-danger">Sim, eu tenho certeza</button>
-                        </div>
-                    </div>
-                </form>
-                <button class="btn btn-success" onclick="window.location.href='/views/eventos.php'">Não, eu quero voltar</button>
-            </div>
+<body class="bg-white">
+    <script>
+    window.onload = function() {
+        
+        swal({
+            title: "Você tem certeza?",
+            text: "O evento será excluído e não vai mais poder ser visto por ninguém!",
+            icon: "warning",
+            buttons: ["Eu não quero fazer isso", "Sim, pode mandar pro limbo!"],
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete) {
+                    swal("Puf! O evento foi excluído :)", {
+                    icon: "success",
+                })
+                    .then((value) => {
+                        document.getElementById('delete-event-form').submit();
+                    }); 
+                } else {
+                    swal("O evento está a salvo, ufa! :)")
+                    .then((value) => {
+                        window.location.href='/views/eventos.php'
+                    });   
+                }
+        });
+
+    }
+    </script>
+<div class="hidden d-none">
+    <form class="w-80 mx-auto" id="delete-event-form" action="../src/controllers/desativarevento.php" method="POST">
+        <div class="form-group">
+                <div class="form-check form-check-inline mt-3">
+                    <input class="form-check-input" type="hidden" name="situacao" value="0" /> <input class="custom-control-input form-check-input" type="checkbox" name="situacao" id="checkbox" value="1" checked>
+                </div>
+                <div class="button mt-5">
+                    <button type="submit" class=" btn btn-danger">Sim, eu tenho certeza</button>
+                </div>
         </div>
-    </div>
+    </form>
+</div>
+ 
 </body>
+    <?php include './partials/scripts.php';?>
 </html>
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
